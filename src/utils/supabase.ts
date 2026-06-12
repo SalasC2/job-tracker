367f6a28959baebb073dd0dc74b3636489a105e8
salasc2 <salasch2@gmail.com>
2026-06-12 12:08:56 -0700

    supabase timeout

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,9 +2,16 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from '../types/database.types';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  db: { schema: 'public' },
+  global: {
+    fetch: (url, options = {}) => {
+      return fetch(url, { ...options, signal: AbortSignal.timeout(5000) })
+    }
+  }
+})
 
 export const signInWithGoogle = async () => {
     await supabase.auth.signInWithOAuth({
